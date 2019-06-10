523c319502780017a81d0a42be972f0cd1d315fa
amlane <twelvexstring@gmail.com>
2019-06-10 14:04:19 -0500

    created user table using migration and set up restricted middle and knexconfig files for global use, set up basic get end point for users without restrict to test

--- api/server.js
+++ api/server.js
@@ -5,14 +5,14 @@ const cors = require('cors');
 const bcrypt = require('bcryptjs');
 
 // const Users = require('../users/users-model.js');
-// const usersRouter = require('../users/users-router.js');
+const usersRouter = require('../users/users-router.js');
 
 const server = express();
 
 server.use(helmet());
 server.use(express.json());
 server.use(cors());
-// server.use('/api/users', usersRouter);
+server.use('/api/users', usersRouter);
 
 server.get('/', (req, res) => {
   res.send("It's alive!");
