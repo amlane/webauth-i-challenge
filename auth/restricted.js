f2cb393b3cac7a566c7e3807741ca3a063d1e4a2
amlane <twelvexstring@gmail.com>
2019-06-11 14:49:45 -0500

    added additional check on restricted middleware to check for req.session.username

--- auth/restricted.js
+++ auth/restricted.js
@@ -3,31 +3,9 @@ const bcrypt = require('bcryptjs');
 const Users = require('../users/users-model.js');
 
 module.exports = function restricted(req, res, next) {
-  if (req.session){
+  if (req.session && req.session.username){
     next();
   } else {
     res.status(401).json({ message: "You shall not pass, restricted middleware! MUHAHAHA!" })
   }
 };
-
-//  module.exports = function restricted(req, res, next) {
-//     // read username and password from the headers and verify them
-//     let { username, password } = req.headers;
-  
-//     if (username && password){
-//       Users.findBy({ username })
-//       .first()
-//       .then(user => {
-//         if(user && bcrypt.compareSync(password, user.password)) {
-//          next();
-//       } else {
-//         res.status(401).json({ message: 'You shall not pass!' });
-//       }
-//     })
-//     .catch(error => {
-//       res.status(500).json(error);
-//     });
-// } else {
-//   res.status(400).json({ message: 'You shall not pass!' });
-// }
-// };
