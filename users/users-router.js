8b1c3f48d204a634cedcb358f50023adbe2cc0dc
amlane <twelvexstring@gmail.com>
2019-06-11 16:07:36 -0500

    sending up final changes for now until web token lesson

--- users/users-router.js
+++ users/users-router.js
@@ -3,7 +3,7 @@ const router = require('express').Router();
 const Users = require('./users-model.js');
 const restricted = require('../auth/restricted.js');
   
-  router.get('/', (req, res) => {
+  router.get('/', (req, res) => {  //need to add restricted back once tokens are added
     Users.find()
       .then(users => {
         res.json(users);
